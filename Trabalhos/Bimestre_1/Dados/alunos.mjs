5c8ff86fdcce31f0579d404e0ccc4ba2ae98b37d
Leonardo Carrijo <lh83712@gmail.com>
2023-09-12 20:55:57 -0300

    Inserção de Novos Alunos

--- Trabalhos/Bimestre_1/Dados/alunos.mjs
+++ Trabalhos/Bimestre_1/Dados/alunos.mjs
@@ -21,5 +21,45 @@ export const alunos = [{
     sexo: 'F',
     media: 8,
     aprovado: true
+},
+{
+    nome: 'Cubinha',
+    ra: '1090482223023',
+    idade: 28,
+    sexo: 'M',
+    media: 10,
+    aprovado: true
+},
+{
+    nome: 'Dimas',
+    ra: '1090482223002',
+    idade: 22,
+    sexo: 'M',
+    media: 8,
+    aprovado: true
+},
+{
+    nome: 'Isadora Helena',
+    ra: '1090482223042',
+    idade: 20,
+    sexo: 'F',
+    media: 9,
+    aprovado: true
+},
+{
+    nome: 'Erick',
+    ra: '1090482223010',
+    idade: 23,
+    sexo: 'M',
+    media: 4,
+    aprovado: false
+},
+{
+    nome: 'Felipe',
+    ra: '1090482223021',
+    idade: 24,
+    sexo: 'M',
+    media: 5,
+    aprovado: false
 }
 ]
--- Trabalhos/Bimestre_1/Scripts/OrdenadorBubble.mjs
+++ Trabalhos/Bimestre_1/Scripts/OrdenadorBubble.mjs
@@ -1,13 +1,18 @@
 import { alunos } from '../Dados/alunos.mjs'
 
+let passadas = 0,comparacoes = 0, trocas = 0
+
 function OrdenarBubble(vetor, fnComp) { // Função que ordena o vetor passado com o BubbleSort
     let swap
     do{
         swap = false
+        passadas++
         for(let i = 0; i < vetor.length - 1; i++){
+            comparacoes++
             if(fnComp(vetor[i], vetor[i + 1])){
                 [vetor[i], vetor[i + 1]] = [vetor[i + 1], vetor[i]]
                 swap = true
+                trocas++
             }
         }
     }while(swap)
@@ -19,3 +24,5 @@ export function OrdenarPorRa(vetor){
 export function OrdenarPorNome(vetor){
     OrdenarBubble(vetor, (obj1, obj2) => obj1.nome > obj2.nome) // Ordena o vetor passado pelo nome de forma crescente
 }
+
+OrdenarBubble
