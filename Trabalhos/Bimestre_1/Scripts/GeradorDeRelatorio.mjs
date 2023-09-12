76accd1f5af1a197e3b7c44e99c77c256bb85c67
Leonardo Carrijo <lh83712@gmail.com>
2023-09-12 20:26:43 -0300

    Gerador de Relatórios

--- Trabalhos/Bimestre_1/Scripts/GeradorDeRelatorio.mjs
+++ Trabalhos/Bimestre_1/Scripts/GeradorDeRelatorio.mjs
@@ -1,9 +1,23 @@
-import { saveAs } from "./fileSaver.mjs";
+import { alunos } from '../Dados/alunos.mjs'
+import { OrdenarBubble } from './OrdenadorBubble.mjs'
 
-function salvar(){
-    let texto = 'Alguma Coisa pra PlaceHolder', titulo = 'Nome do Arquivo'
-    let blob = new Blob([texto],{
-        type: 'text/plain;charset=utf-8'
-    });
-    saveAs(blob, titulo + '.txt')
+
+export function GerarRelatorio(vetor){
+    for(let i = 0; i < vetor.length; i++){
+        console.log(`${i + 1}º Aluno:`)
+        console.log(`   Nome: ${alunos[i].nome}`)
+        console.log(`   RA: ${alunos[i].ra}`)
+        console.log(`   Idade: ${alunos[i].idade}`)
+        if(alunos[i].sexo == 'M'){
+            console.log('   Masculino')
+        }else{
+            console.log('   Feminino')
+        }
+        console.log(`   Média: ${alunos[i].media}`)
+        if(alunos[i].aprovado){
+            console.log('   Aprovado\n')
+        }else{
+            console.log('   Reprovado\n')
+        }
+    }
 }
