a1fd68f2f90c1ba73ba0f0ef1be01af73f2a4cb1
Leonardo Carrijo <lh83712@gmail.com>
2023-09-13 21:32:00 -0300

    SelectionSort
    (Não funcional nos Alunos)

--- Trabalhos/Bimestre_1/Scripts/OrdenacaoSelection.mjs
+++ Trabalhos/Bimestre_1/Scripts/OrdenacaoSelection.mjs
@@ -1,6 +1,26 @@
 import { alunos } from "../Dados/alunos.mjs";
 
-function OrdenarSelection(vetor){
-    let menor = vetor[0].nome
-    
+let passadas = 0, comparacoes = 0, trocas = 0
+
+function OrdenarSelection(vetor, fnComp){
+    for(let posSel = 0; posSel < vetor.length; posSel++){
+        let posMenor = posSel + 1
+        passadas++
+        for(let i = posMenor + 1; i < vetor.length; i++) {
+            comparacoes++
+            if(vetor[posMenor] > vetor[i]){
+                posMenor = i
+            }     
+        }
+        if(fnComp(vetor[posSel], vetor[posMenor])){
+            [vetor[posSel], vetor[posMenor]] = [vetor[posMenor], vetor[posSel]]
+            trocas++
+        }
+    }
 }
+
+let numeros = [77, 99, 11, 0, 22, 55, 33, 44, 66, 88]
+
+OrdenarSelection(alunos, (obj1, obj2) => obj1.nome > obj2.nome)
+console.log(alunos)
+console.log(`Passadas: ${passadas}\nComparações: ${comparacoes}\nTrocas: ${trocas}`)
