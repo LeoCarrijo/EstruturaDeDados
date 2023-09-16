c870e1732a2114177b5b56db335a585ec438f81b
Leonardo Carrijo <lh83712@gmail.com>
2023-09-16 14:30:08 -0300

    Merge

--- Trabalhos/Bimestre_1/Scripts/OrdenacaoMerge.mjs
+++ Trabalhos/Bimestre_1/Scripts/OrdenacaoMerge.mjs
@@ -1,5 +1,5 @@
 function OrdenarMerge(vetor){
-    if(vetor.length == 1) return 1
+    if(vetor.length == 1) return vetor
 
     let meio = Math.floor(vetor.length / 2)
 
@@ -10,8 +10,28 @@ function OrdenarMerge(vetor){
 
     vetorEsquerdo = OrdenarMerge(vetorEsquerdo)
     vetorDireito = OrdenarMerge(vetorDireito)
+    let menorValor = [], maiorValor = []
+    if(vetorEsquerdo[0] <= vetorDireito[0]) {
+        menorValor = vetorEsquerdo[0]
+        maiorValor = vetorDireito[0]
+    }else{
+        menorValor = vetorDireito[0]
+        maiorValor = vetorEsquerdo[0]
+    }
+    let vetorTotal = [menorValor, maiorValor]
+    console.log(vetorTotal)
+    return vetorTotal
 }
 
-let numeros = [77, 99, 11, 0, 22, 55, 33, 44, 66, 88]
+let numeros = [99, 77, 11, 0, 22, 55, 33, 44, 66, 88]
 
 OrdenarMerge(numeros)
+
+// function imc(){
+//     var sexo = ''
+//     if(document.getElementById('inSexoM').value == true){
+//         sexo = 'Masculino'
+//     }else{
+//         sexo = 'Feminino'
+//     }
+// }
