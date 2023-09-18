fa6b52e1219ad132a1cf58d7a8975d7814f2cb51
Leonardo Carrijo <lh83712@gmail.com>
2023-09-18 20:31:44 -0300

    MergeSort

---
+++ Trabalhos/Bimestre_1/Scripts/MergeSort.mjs
@@ -0,0 +1,30 @@
+function MergeSort(vetor) {
+    if(vetor.length <= 1) return vetor
+
+    let meio = Math.floor(vetor.length / 2)
+    let vetorEsquerdo = vetor.slice(0, meio)
+    let vetorDireito = vetor.slice(meio)
+
+    return Merge(MergeSort(vetorEsquerdo), MergeSort(vetorDireito))
+}
+
+function Merge(vetorEsquerdo, vetorDireito) {
+    let vetorFinal = []
+    let iEsquerdo = 0
+    let iDireito = 0
+
+    while(iEsquerdo < vetorEsquerdo.length && iDireito < vetorDireito){
+        if(vetorEsquerdo[iEsquerdo] < vetorDireito[iDireito]){
+            vetorFinal.push(vetorEsquerdo)
+            iEsquerdo++
+        }else{
+            vetorFinal.push(vetorDireito)
+            iDireito++
+        }
+    }
+    return vetorFinal.concat(vetorEsquerdo.slice(iEsquerdo), vetorDireito.slice(iDireito))
+}
+
+const vetor = [12, 11, 13, 5, 6, 7]
+const vetorOrdenado = MergeSort(vetor)
+console.log(vetorOrdenado)
