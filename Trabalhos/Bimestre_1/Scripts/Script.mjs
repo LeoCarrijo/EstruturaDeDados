8c96d5b00649c651f3bd71b6c4b488a478bc0fc2
Leonardo Carrijo <lh83712@gmail.com>
2023-09-17 22:18:10 -0300

    Comentários no Código

--- Trabalhos/Bimestre_1/Scripts/Script.mjs
+++ Trabalhos/Bimestre_1/Scripts/Script.mjs
@@ -1,4 +1,4 @@
-const alunos = [{
+const alunos = [{ // Lista que armazena os alunos
     nome: 'Leonardo Henrique Carrijo',
     ra: '1090482223007',
     idade: 20,
@@ -71,7 +71,7 @@ const alunos = [{
     aprovado: true
 }
 ]
-let elementos = {
+let elementos = { // Objeto que armazena os elementos HTML
     nome: document.querySelector('input#nome'),
     ra: document.querySelector('input#ra'),
     idade: document.querySelector('input#idade'),
@@ -81,44 +81,31 @@ let elementos = {
     tabelaRelatorio: document.querySelector('table.tabela__relatorio')
 }
 
-function ResetarValores(){
-    elementos.nome.value = ''
-    elementos.ra.value = ''
-    elementos.idade.value = ''
-    elementos.sexoM.checked = true
-    elementos.media.value = ''
-    elementos.aprovado.checked = false
-}
-function OrdenarBubble(vetor, fnComp) { // Função que ordena o vetor passado com o BubbleSort
-    let swap, passadas = 0, comparacoes = 0, trocas = 0
+function OrdenarBubble(vetor, fnComp) { // Função que ordena o vetor passado com o BubbleSort e é usada para gerar todas as outras ordenações
     do{
         swap = false
-        passadas++
         for(let i = 0; i < vetor.length - 1; i++){
-            comparacoes++
             if(fnComp(vetor[i], vetor[i + 1])){
                 [vetor[i], vetor[i + 1]] = [vetor[i + 1], vetor[i]]
-                swap = true
-                trocas++
+                swap = true // Flag que basicamente fala ao programa "Caso eu trocar algo aqui, continue rodando, até que eu não troque nada (ou seja, "swap = false"), pois aí quer dizer que a lista já está ordenada
             }
         }
     }while(swap)
-    console.log(`Passadas: ${passadas}\nComparações: ${comparacoes}\nTrocas: ${trocas}`)
 }
-function OrdenarPorRa(vetor){
+function OrdenarPorRa(vetor){ // Função que chama a função que ordena a lista e passa os parametros que ordenam o por RA decrescente e a função que printa a tabela na tela
     OrdenarBubble(vetor, (obj1, obj2) => obj1.ra < obj2.ra) // Ordena o vetor passado pelo ra de forma decrescente
     GerarRelatorio(alunos, false)
 }
-function OrdenarPorNome(vetor){
+function OrdenarPorNome(vetor){ // Função que chama a função que ordena a lista e passa os parametros que ordenam o por nome crescente e a função que printa a tabela na tela
     OrdenarBubble(vetor, (obj1, obj2) => obj1.nome > obj2.nome) // Ordena o vetor passado pelo nome de forma crescente
     GerarRelatorio(alunos, false)
 }
-function OrdenarPorNomeAprovados(vetor) {
+function OrdenarPorNomeAprovados(vetor) { // Função que chama a função que ordena a lista e passa os parametros que ordenam o por nome crescente e apenas os aprovados e a função que printa a tabela na tela
     OrdenarBubble(vetor, (obj1, obj2) => obj1.nome > obj2.nome)
     GerarRelatorio(alunos, true)
 }
-function EnviarDados() {
-    let Aluno = {
+function EnviarDados() { // Função que é usada para enviar os dados para as outras funções do programa pelo botão "Cadastrar Aluno"
+    let Aluno = { // Cria o objeto que vai ser adicionado à lista de alunos
         nome: elementos.nome.value,
         ra: elementos.ra.value,
         idade: elementos.idade.value,
@@ -126,35 +113,34 @@ function EnviarDados() {
         media: elementos.media.value,
         aprovado: elementos.aprovado.checked
     }
-    Cadastrar(Aluno)
-    alert(`Aluno ${alunos[alunos.length - 1].nome} Cadastrado!`)
-    GerarRelatorio(alunos, false)
-    ResetarValores()
+    Cadastrar(Aluno) // Chama a função que coloca o objeto na lista
+    alert(`Aluno ${alunos[alunos.length - 1].nome} Cadastrado!`) // Da o feedback ao usuário de que o aluno foi cadastrado
+    ResetarValores() // limpa os campos de Input
 }
 function Cadastrar(aluno) { // Função que recebe um aluno e o inclui na lista
     alunos.push(aluno)
 }
-function GerarRelatorio(vetor, apenasAprovados){ // Função que gera os relatórios dos alunos
-    elementos.tabelaRelatorio.innerHTML = ''
-    GerarLinhaCabecalho()
-    for(let i = 0; i < vetor.length; i++){
-        if(!apenasAprovados){
+function GerarRelatorio(vetor, apenasAprovados){ // Função que é utilizada para gerar todos os tipos de relatórios
+    elementos.tabelaRelatorio.innerHTML = '' // Apaga a tabela existente para criar a nova tabela ordenada
+    GerarLinhaCabecalho() // Chama a função que gera a linha de cabeçalho
+    for(let i = 0; i < vetor.length; i++){ 
+        if(!apenasAprovados){ // Verifica se foi colocado para todos os alunos forem mostrados ou apenas aprovados
             PrintarAluno(vetor, i)
-        }else{
+        }else{ 
             if(alunos[i].aprovado){
                 PrintarAluno(vetor, i)
             }
         }
     }
 }
-function PrintarAluno(vetor, i){
-    let tabela = elementos.tabelaRelatorio
-    const linha = document.createElement('tr')
-    tabela.appendChild(linha)
-    const nome = document.createElement('td')
-    linha.appendChild(nome)
-    nome.textContent = vetor[i].nome
-    const ra = document.createElement('td')
+function PrintarAluno(vetor, i){ // Função usada pela Função "GerarRelatorio()" para criar as células do aluno adicionado à tabela no HTML
+    let tabela = elementos.tabelaRelatorio // Cria uma variável local para o elemento da tabela
+    const linha = document.createElement('tr') // Cria uma linha na tabela
+    tabela.appendChild(linha) // Atrela essa linha na tebela
+    const nome = document.createElement('td') // Cria a célula do valor do nome do aluno
+    linha.appendChild(nome) // Atrela essa célula à linha que está atrelada à tabela
+    nome.textContent = vetor[i].nome // Escreve o nome do aluno em questão na célula
+    const ra = document.createElement('td') // Em seguida o código se repete para os outros atributos dos alunos na lista
     linha.appendChild(ra)
     ra.textContent = vetor[i].ra
     const sexo = document.createElement('td')
@@ -167,7 +153,7 @@ function PrintarAluno(vetor, i){
     linha.appendChild(aprovado)
     aprovado.textContent = vetor[i].aprovado ? 'Aprovado' : 'Reprovado'
 }
-function GerarLinhaCabecalho(){
+function GerarLinhaCabecalho(){ // Função que faz quase a mesma coisa que a "PrintarAluno()" porém cria apenas a primeira linha da tabela
     const tabelaCabecalho = document.createElement('tr')
     elementos.tabelaRelatorio.appendChild(tabelaCabecalho)
     const nome = document.createElement('th')
@@ -185,4 +171,12 @@ function GerarLinhaCabecalho(){
     const aprovado = document.createElement('th')
     tabelaCabecalho.appendChild(aprovado)
     aprovado.textContent = "Resultado"
+}
+function ResetarValores(){ // Função que reseta os valores dos inputs para o valor padrão
+    elementos.nome.value = ''
+    elementos.ra.value = ''
+    elementos.idade.value = ''
+    elementos.sexoM.checked = true
+    elementos.media.value = ''
+    elementos.aprovado.checked = false
 }
